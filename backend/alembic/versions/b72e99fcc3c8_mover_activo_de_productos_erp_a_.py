5a6d8e8e2b2a0a0061db69e3ef5c527da4b16b2d
Fernando <sernafernando@gmail.com>
2025-10-01 11:04:30 -0300

    update

---
+++ backend/alembic/versions/b72e99fcc3c8_mover_activo_de_productos_erp_a_.py
@@ -0,0 +1,38 @@
+"""mover activo de productos_erp a publicaciones_ml
+
+Revision ID: b72e99fcc3c8
+Revises: d19958be66a1
+Create Date: 2025-10-01 11:03:28.626765
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b72e99fcc3c8'
+down_revision: Union[str, None] = 'd19958be66a1'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    # Quitar 'activo' de productos_erp
+    op.drop_column('productos_erp', 'activo')
+
+    # Agregar 'activo' en publicaciones_ml
+    op.add_column(
+        'publicaciones_ml',
+        sa.Column('activo', sa.Boolean(), nullable=False, server_default=sa.text('true'))
+    )
+
+
+def downgrade():
+    # Revertir cambios
+    op.add_column(
+        'productos_erp',
+        sa.Column('activo', sa.Boolean(), nullable=False, server_default=sa.text('true'))
+    )
+    op.drop_column('publicaciones_ml', 'activo')
