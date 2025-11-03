2920c4d21de5920b081b3d254f479521cc3a1c29
Fernando <sernafernando@gmail.com>
2025-11-03 10:58:57 -0300

    fix pm filter

--- backend/create_marcas_pm_table.sql
+++ backend/create_marcas_pm_table.sql
@@ -13,7 +13,7 @@ CREATE INDEX idx_marcas_pm_usuario_id ON marcas_pm(usuario_id);
 
 -- Insertar todas las marcas existentes sin asignar
 INSERT INTO marcas_pm (marca, usuario_id)
-SELECT DISTINCT marca, NULL
+SELECT DISTINCT marca, NULL::INTEGER
 FROM productos_erp
 WHERE marca IS NOT NULL
 ON CONFLICT (marca) DO NOTHING;
