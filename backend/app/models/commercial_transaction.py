22f62a0c06bf9f31e1ec2b2f6030c334e1f966ac
Fernando <sernafernando@gmail.com>
2025-11-04 08:53:50 -0300

    fix script ct

--- backend/app/models/commercial_transaction.py
+++ backend/app/models/commercial_transaction.py
@@ -7,6 +7,9 @@ class CommercialTransaction(Base):
     """
     Modelo para tbCommercialTransactions del ERP
     Contiene todas las transacciones comerciales (ventas, compras, remitos, etc.)
+
+    IMPORTANTE: Los nombres de atributos mantienen camelCase para compatibilidad con el código,
+    pero se mapean a nombres en minúsculas en PostgreSQL usando el parámetro 'name'
     """
     __tablename__ = "tb_commercial_transactions"
 
@@ -14,17 +17,17 @@ class CommercialTransaction(Base):
     comp_id = Column(Integer)
     bra_id = Column(Integer)
     ct_transaction = Column(BigInteger, primary_key=True, index=True)
-    ct_pointOfSale = Column(Integer)
-    ct_kindOf = Column(String(10), index=True)
-    ct_docNumber = Column(String(50), index=True)
+    ct_pointOfSale = Column('ct_pointofsale', Integer)
+    ct_kindOf = Column('ct_kindof', String(10), index=True)
+    ct_docNumber = Column('ct_docnumber', String(50), index=True)
 
     # Fechas
     ct_date = Column(DateTime, index=True)
-    ct_taxDate = Column(DateTime)
-    ct_payDate = Column(DateTime)
-    ct_deliveryDate = Column(DateTime)
-    ct_processingDate = Column(DateTime)
-    ct_lastPayDate = Column(DateTime)
+    ct_taxDate = Column('ct_taxdate', DateTime)
+    ct_payDate = Column('ct_paydate', DateTime)
+    ct_deliveryDate = Column('ct_deliverydate', DateTime)
+    ct_processingDate = Column('ct_processingdate', DateTime)
+    ct_lastPayDate = Column('ct_lastpaydate', DateTime)
     ct_cd = Column(DateTime)
 
     # Relaciones
@@ -43,25 +46,25 @@ class CommercialTransaction(Base):
     ct_discount = Column(Numeric(18, 2))
     ct_adjust = Column(Numeric(18, 2))
     ct_taxes = Column(Numeric(18, 2))
-    ct_ATotal = Column(Numeric(18, 2))
-    ct_ABalance = Column(Numeric(18, 2))
-    ct_AAdjust = Column(Numeric(18, 2))
-    ct_inCash = Column(Numeric(18, 2))
-    ct_optionalValue = Column(Numeric(18, 2))
-    ct_documentTotal = Column(Numeric(18, 2))
+    ct_ATotal = Column('ct_atotal', Numeric(18, 2))
+    ct_ABalance = Column('ct_abalance', Numeric(18, 2))
+    ct_AAdjust = Column('ct_aadjust', Numeric(18, 2))
+    ct_inCash = Column('ct_incash', Numeric(18, 2))
+    ct_optionalValue = Column('ct_optionalvalue', Numeric(18, 2))
+    ct_documentTotal = Column('ct_documenttotal', Numeric(18, 2))
 
     # Monedas y tipos de cambio
     curr_id_transaction = Column(Integer)
-    ct_ACurrency = Column(Integer)
-    ct_ACurrencyExchange = Column(Numeric(18, 10))
-    ct_CompanyCurrency = Column(Integer)
-    ct_Branch2CompanyCurrencyExchange = Column(Numeric(18, 10))
-    ct_dfExchange = Column(Numeric(18, 10))
-    curr_id4Exchange = Column(Integer)
-    ct_curr_IdExchange = Column(Numeric(18, 10))
-    curr_id4dfExchange = Column(Integer)
-    ct_dfExchangeOriginal = Column(Numeric(18, 10))
-    ct_curr_IdExchangeOriginal = Column(Numeric(18, 10))
+    ct_ACurrency = Column('ct_acurrency', Integer)
+    ct_ACurrencyExchange = Column('ct_acurrencyexchange', Numeric(18, 10))
+    ct_CompanyCurrency = Column('ct_companycurrency', Integer)
+    ct_Branch2CompanyCurrencyExchange = Column('ct_branch2companycurrencyexchange', Numeric(18, 10))
+    ct_dfExchange = Column('ct_dfexchange', Numeric(18, 10))
+    curr_id4Exchange = Column('curr_id4exchange', Integer)
+    ct_curr_IdExchange = Column('ct_curr_idexchange', Numeric(18, 10))
+    curr_id4dfExchange = Column('curr_id4dfexchange', Integer)
+    ct_dfExchangeOriginal = Column('ct_dfexchangeoriginal', Numeric(18, 10))
+    ct_curr_IdExchangeOriginal = Column('ct_curr_idexchangeoriginal', Numeric(18, 10))
 
     # Referencias contables
     hacc_transaction = Column(Integer)
@@ -78,49 +81,49 @@ class CommercialTransaction(Base):
     state_id = Column(Integer)
 
     # Estados y flags
-    ct_Pending = Column(Boolean, default=True)
-    ct_isAvailableForImport = Column(Boolean, default=False)
-    ct_isAvailableForPayment = Column(Boolean, default=False)
-    ct_isCancelled = Column(Boolean, default=False)
-    ct_isSelected = Column(Boolean, default=False)
-    ct_isMigrated = Column(Boolean, default=False)
-    ct_powerBoardOK = Column(Boolean, default=False)
-    ct_Fiscal_Check4EmptyNumbers = Column(Boolean, default=False)
-    ct_DisableExchangeDifferenceInterest = Column(Boolean, default=False)
+    ct_Pending = Column('ct_pending', Boolean, default=True)
+    ct_isAvailableForImport = Column('ct_isavailableforimport', Boolean, default=False)
+    ct_isAvailableForPayment = Column('ct_isavailableforpayment', Boolean, default=False)
+    ct_isCancelled = Column('ct_iscancelled', Boolean, default=False)
+    ct_isSelected = Column('ct_isselected', Boolean, default=False)
+    ct_isMigrated = Column('ct_ismigrated', Boolean, default=False)
+    ct_powerBoardOK = Column('ct_powerboardok', Boolean, default=False)
+    ct_Fiscal_Check4EmptyNumbers = Column('ct_fiscal_check4emptynumbers', Boolean, default=False)
+    ct_DisableExchangeDifferenceInterest = Column('ct_disableexchangedifferenceinterest', Boolean, default=False)
 
     # Ventas especiales / ecommerce
     ct_soh_bra_id = Column(Integer)
     ct_soh_id = Column(Integer)
-    ct_transaction_PackingList = Column(String(100))
-    ws_cust_Id = Column(Integer)
-    ws_internalID = Column(String(100))
-    ws_isLiquidated = Column(Boolean, default=False)
-    ws_isLiquidatedCD = Column(Boolean, default=False)
-    ws_is4Liquidation = Column(Boolean, default=False)
-    ws_LiquidationNumber = Column(String(100))
+    ct_transaction_PackingList = Column('ct_transaction_packinglist', String(100))
+    ws_cust_Id = Column('ws_cust_id', Integer)
+    ws_internalID = Column('ws_internalid', String(100))
+    ws_isLiquidated = Column('ws_isliquidated', Boolean, default=False)
+    ws_isLiquidatedCD = Column('ws_isliquidatedcd', Boolean, default=False)
+    ws_is4Liquidation = Column('ws_is4liquidation', Boolean, default=False)
+    ws_LiquidationNumber = Column('ws_liquidationnumber', String(100))
     ws_st_id = Column(Integer)
     ws_dl_id = Column(Integer)
 
     # AFIP / Fiscales
-    ct_CAI = Column(String(100))
-    ct_CAIDate = Column(DateTime)
-    ct_FEIdNumber = Column(String(100))
+    ct_CAI = Column('ct_cai', String(100))
+    ct_CAIDate = Column('ct_caidate', DateTime)
+    ct_FEIdNumber = Column('ct_feidnumber', String(100))
 
     # Descuentos detallados
-    ct_Discount1 = Column(Numeric(18, 2), default=0)
-    ct_Discount2 = Column(Numeric(18, 2), default=0)
-    ct_Discount3 = Column(Numeric(18, 2), default=0)
-    ct_Discount4 = Column(Numeric(18, 2), default=0)
-    ct_discountPlanCoeficient = Column(Numeric(18, 10), default=1)
+    ct_Discount1 = Column('ct_discount1', Numeric(18, 2), default=0)
+    ct_Discount2 = Column('ct_discount2', Numeric(18, 2), default=0)
+    ct_Discount3 = Column('ct_discount3', Numeric(18, 2), default=0)
+    ct_Discount4 = Column('ct_discount4', Numeric(18, 2), default=0)
+    ct_discountPlanCoeficient = Column('ct_discountplancoeficient', Numeric(18, 10), default=1)
 
     # Paquetes y envíos
-    ct_packagesQty = Column(Integer, default=0)
-    ct_TransactionBarCode = Column(String(100))
+    ct_packagesQty = Column('ct_packagesqty', Integer, default=0)
+    ct_TransactionBarCode = Column('ct_transactionbarcode', String(100))
 
     # Créditos
-    ct_CreditIntPercentage = Column(Numeric(10, 4), default=0)
-    ct_CreditIntPlusPercentage = Column(Numeric(10, 4), default=0)
-    ctl_daysPastDue = Column(Integer, default=0)
+    ct_CreditIntPercentage = Column('ct_creditintpercentage', Numeric(10, 4), default=0)
+    ct_CreditIntPlusPercentage = Column('ct_creditintpluspercentage', Numeric(10, 4), default=0)
+    ctl_daysPastDue = Column('ctl_dayspastdue', Integer, default=0)
     ccp_id = Column(Integer)
 
     # Otros IDs de referencia
@@ -129,21 +132,21 @@ class CommercialTransaction(Base):
     pt_id = Column(Integer)
     fc_id = Column(Integer)
     pro_id = Column(Integer)
-    supp_id4CreditCardLiquidation = Column(Integer)
+    supp_id4CreditCardLiquidation = Column('supp_id4creditcardliquidation', Integer)
     def_id = Column(Integer)
     mlo_id = Column(Integer)
     dc_id = Column(Integer)
 
     # Campos de uso general (tags)
-    ct_AllUseTag1 = Column(String(255))
-    ct_AllUseTag2 = Column(String(255))
-    ct_AllUseTag3 = Column(String(255))
-    ct_AllUseTag4 = Column(String(255))
+    ct_AllUseTag1 = Column('ct_allusetag1', String(255))
+    ct_AllUseTag2 = Column('ct_allusetag2', String(255))
+    ct_AllUseTag3 = Column('ct_allusetag3', String(255))
+    ct_AllUseTag4 = Column('ct_allusetag4', String(255))
 
     # GUID y documentación
     ct_guid = Column(UUID(as_uuid=True), index=True)
-    ct_transaction4ThirdSales = Column(String(100))
-    ct_documentNumber = Column(String(100))
+    ct_transaction4ThirdSales = Column('ct_transaction4thirdsales', String(100))
+    ct_documentNumber = Column('ct_documentnumber', String(100))
     ct_note = Column(Text)
 
     # Auditoría local
