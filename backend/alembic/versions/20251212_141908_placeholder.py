7d03582bf52792c20e72d90289422396ed91cc9e
Fernando Serna <sernafernando@gmail.com>
2025-12-12 14:41:22 -0300

    fix: tab en tienda

---
+++ backend/alembic/versions/20251212_141908_placeholder.py
@@ -0,0 +1,26 @@
+"""placeholder migration
+
+Revision ID: 20251212_141908
+Revises: create_permisos_system
+Create Date: 2025-12-12 14:19:08
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20251212_141908'
+down_revision = 'create_permisos_system'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Migración placeholder - ya fue aplicada manualmente"""
+    pass
+
+
+def downgrade():
+    """Migración placeholder - ya fue aplicada manualmente"""
+    pass
